2790a988c9aec5aa05d8501e54de8d8aa59191de
Tu <hoangtrongtu960@gmail.com>
2024-04-14 22:28:20 +0700

    Second, demo done

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from ultralytics import YOLO
-
+from api import print_recipes
 model = YOLO("best.onnx", task="detect")
 
 results = model("image.jpg")
+ingredients = ""
 
 for result in results:
     boxes = result.boxes
     names = result.names
+    list = [0] * 100
     for i in boxes.cls:
-        print(names[int(i.item())])
+        list[int(i.item())] += 1
+    for j in range(0, 100):
+        if list[j] != 0:
+            print(list[j]," ", names[j])
+            ingredients += names[j] + ",+"
+
+print_recipes(ingredients[:-2])
